b7e46371f1a7b7197294f867e087cb7469948193
ramaganesh <36730709+ramaganesh@users.noreply.github.com>
2018-08-30 14:50:22 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,9 @@
+alphabet="a,e,i,o,u"
+n=input()
+if(n in alphabet):
+    print("vowel")
+con="b,c,d,f,g,h,j,k,l,m,n,p,q,r,s,t,v,w,x,y,z"    
+if (n in con):
+    print("consonant")
+else:
+    print ("invalid")
